65976115ed506c86e7fd6a4eabca056fa4928799
xingyun <xingyun@DESKTOP-6L24LNU.lan>
2021-05-13 22:16:36 +0900

    [update]

--- week1/homework2/FindAnagramUpdated.java
+++ week1/homework2/FindAnagramUpdated.java
@@ -1,17 +1,55 @@
 package week1;
 
-public class FindAnagramUpdated2 {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
-	static String filename;
+public class FindAnagramUpdated {
+
+	static String wordsfile;
 
 	public static void main(String[] args) {
-		//ファイルから読み込み
+
+		System.out.println("input file name\n");
+		//単語ファイルの指定
+		wordsfile = args[0];
 
 		//実行
+		find_higher_score();
 
 	}
 
-	static void FindHigherScore() {
+	static void find_higher_score() {
+
+		//単語ファイルから読み込み
+		try {
+	    	File file = new File(wordsfile);
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			while (br.ready()) {
+				String s = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+		     e.printStackTrace();
+	    }
+	}
+
+	static int[] count_char(String x) {
+		int count []= new int[26];
+		char c[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+			      'v', 'w', 'x', 'y', 'z' };
+		for(int i = 0; i < x.length(); i++) {
+			for(int j = 0; j < 26; j++) {
+				if(x.charAt(i) == c[j]) {
+					count[j]++;
+				}
+			}
+		}
+		return count;
+	}
+
+	int charScore[] = {1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4};
 
 		//ファイルから文字列に
 		//文字列からcharに
@@ -20,5 +58,5 @@ public class FindAnagramUpdated2 {
 		//点数が高いものがあるか判定
 		//更新
 		//新しいファイルに出力
-	}
+
 }
