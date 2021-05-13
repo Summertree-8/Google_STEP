ed83b7619dcd89249f6fecb43bb7755afe063230
xingyun <xingyun@DESKTOP-6L24LNU.lan>
2021-05-13 23:51:15 +0900

    [update]

--- week1/homework2/FindAnagramUpdated.java
+++ week1/homework2/FindAnagramUpdated.java
@@ -1,8 +1,19 @@
+//かなり混沌としています。ごめんなさい、、！
+
 package week1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class FindAnagramUpdated {
 
@@ -21,6 +32,8 @@ public class FindAnagramUpdated {
 
 	static void find_higher_score() {
 
+		ArrayList<String> random_words = new ArrayList<String>();
+		ArrayList<String> anagram = new ArrayList<String>();
 		//単語ファイルから読み込み
 		try {
 	    	File file = new File(wordsfile);
@@ -28,22 +41,45 @@ public class FindAnagramUpdated {
 			BufferedReader br = new BufferedReader(fr);
 			while (br.ready()) {
 				String s = br.readLine();
-				// 辞書の各単語に対してアナグラムを見つける
-				for() {
-					find_anagram(,)
-				}
+				random_words.add(s);
 			}
 			br.close();
 		} catch (Exception e) {
 		     e.printStackTrace();
 	    }
-		//新しいファイルに出力
+
+		// 辞書の各単語に対してアナグラムを見つける
+		for() {
+			find_anagram(random_words,)
+		}
 	}
 
+	//アナグラムであれば出力
 	static void find_anagram(String ramdom_word, String dictionary_word) {
+		int i = 0;
 
+		for(int j = 0; j < ramdom_word.length(); j++) {
+			if(ramdom_word.charAt(i) != dictionary_word.charAt(i)) {
+				break;
+			}
+			i++;
+		}
+		if(i == ramdom_word.length()) {
+			//新しいファイルに出力
+			try{
+			    File file = new File("new"+wordsfile+".txt");
+			    FileWriter filewriter = new FileWriter(file);
+			    BufferedWriter bw = new BufferedWriter(filewriter);
+			    String s = dictionary_word;
+			    anagram.add(s);
+			    filewriter.close();
+			}catch(IOException e){
+			     System.out.println(e);
+			}
+	    }
 	}
 
+	//アルファベットも個数をカウント
 	static int[] count_char(String x) {
 		int count []= new int[26];
 		char c[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
